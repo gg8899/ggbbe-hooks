a2e5e8dfe6ac2f2faed177aacc6c04e411c22846
GUOKKK <528032175@qq.com>
2024-03-09 23:44:23 +0800

    重新搭建

--- packages/hooks/src/__tests__/index.test.ts
+++
@@ -1,9 +0,0 @@
-import * as ggbeeHooks from '..';
-
-describe('ggbeeHooks', () => {
-  test('export modules should be define', () => {
-    Object.keys(ggbeeHooks).forEach((module) => {
-      expect(ggbeeHooks[module]).toBeDefined();
-    });
-  });
-});
